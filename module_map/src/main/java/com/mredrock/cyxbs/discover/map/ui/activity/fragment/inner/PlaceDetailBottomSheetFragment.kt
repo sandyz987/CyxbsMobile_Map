a6e791ce579737ea85147e10c896557dfc4dd2e1
737138090 <737138090@qq.com>
2020-08-12 14:13:27 +0800

    [feature]添加地点详情页的描述，关于该地点

--- module_map/src/main/java/com/mredrock/cyxbs/discover/map/ui/activity/fragment/inner/PlaceDetailBottomSheetFragment.kt
+++ module_map/src/main/java/com/mredrock/cyxbs/discover/map/ui/activity/fragment/inner/PlaceDetailBottomSheetFragment.kt
@@ -2,6 +2,7 @@ package com.mredrock.cyxbs.discover.map.ui.activity.fragment.inner
 
 import android.graphics.Color
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,9 +10,13 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.beloo.widget.chipslayoutmanager.ChipsLayoutManager
 import com.mredrock.cyxbs.common.utils.extensions.dp2px
 import com.mredrock.cyxbs.discover.map.R
 import com.mredrock.cyxbs.discover.map.databinding.MapFragmentPlaceDetailContainerBinding
+import com.mredrock.cyxbs.discover.map.ui.activity.adapter.DetailAttributeRvAdapter
+import com.mredrock.cyxbs.discover.map.ui.activity.adapter.DetailTagRvAdapter
 import com.mredrock.cyxbs.discover.map.ui.activity.adpter.BannerRvAdapter
 import com.mredrock.cyxbs.discover.map.viewmodel.MapViewModel
 import com.to.aboomy.pager2banner.IndicatorView
@@ -32,6 +37,7 @@ class PlaceDetailBottomSheetFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(requireActivity()).get(MapViewModel::class.java)
+
         val indicator = IndicatorView(context)
                 .setIndicatorColor(Color.DKGRAY)
                 .setIndicatorSelectorColor(Color.WHITE)
@@ -44,6 +50,25 @@ class PlaceDetailBottomSheetFragment : Fragment() {
         map_banner_detail_image.setIndicator(indicator).setPageMargin(context?.dp2px(20f)
                 ?: 0, context?.dp2px(10f)
                 ?: 0).addPageTransformer(ScaleInTransformer()).adapter = bannerAdapter
+
+        val chipsLayoutManager = ChipsLayoutManager.newBuilder(context)
+                .setChildGravity(Gravity.TOP)
+                .setScrollingEnabled(true)
+                .setMaxViewsInRow(2)
+                .setGravityResolver { Gravity.START }
+                .setRowBreaker { position -> position == 6 || position == 11 || position == 2 }
+                .setOrientation(ChipsLayoutManager.HORIZONTAL)
+                .setRowStrategy(ChipsLayoutManager.STRATEGY_DEFAULT)
+                .withLastRow(true)
+                .build()
+        map_rv_detail_about_list.layoutManager = chipsLayoutManager
+        val tagAdapter = context?.let { DetailTagRvAdapter(it, mutableListOf()) }
+        map_rv_detail_about_list.adapter = tagAdapter
+
+        val linearLayoutManager = LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL,false)
+        map_rv_detail_place_attribute.layoutManager = linearLayoutManager
+        val attributeAdapter = context?.let { DetailAttributeRvAdapter(it, mutableListOf()) }
+        map_rv_detail_place_attribute.adapter = attributeAdapter
         /**
          * 对要显示的内容监听
          */
@@ -55,6 +80,14 @@ class PlaceDetailBottomSheetFragment : Fragment() {
                         bannerAdapter.setList(t.images)
                         bannerAdapter.notifyDataSetChanged()
                         map_banner_detail_image.adapter = bannerAdapter
+                    }
+                    if (tagAdapter != null) {
+                        tagAdapter.setList(t.tags)
+                        tagAdapter.notifyDataSetChanged()
+                    }
+                    if (attributeAdapter != null) {
+                        attributeAdapter.setList(t.tags)
+                        attributeAdapter.notifyDataSetChanged()
                     }
                 }
         )
