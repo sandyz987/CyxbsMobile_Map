50dc8b79c4e83a4e6a3dfa5ebd8ecf3cbf2ee530
737138090 <737138090@qq.com>
2020-08-17 13:40:01 +0800

    [optimize]删除banner第三方库，手写banner

--- module_map/src/main/java/com/mredrock/cyxbs/discover/map/ui/inner/PlaceDetailBottomSheetFragment.kt
+++ module_map/src/main/java/com/mredrock/cyxbs/discover/map/ui/inner/PlaceDetailBottomSheetFragment.kt
@@ -1,6 +1,5 @@
 package com.mredrock.cyxbs.discover.map.ui.inner
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,18 +11,16 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.flexbox.FlexWrap
 import com.google.android.flexbox.FlexboxLayoutManager
-import com.mredrock.cyxbs.common.utils.extensions.dp2px
 import com.mredrock.cyxbs.common.utils.extensions.gone
 import com.mredrock.cyxbs.common.utils.extensions.visible
 import com.mredrock.cyxbs.discover.map.R
 import com.mredrock.cyxbs.discover.map.bean.FavoritePlace
 import com.mredrock.cyxbs.discover.map.databinding.MapFragmentPlaceDetailContainerBinding
-import com.mredrock.cyxbs.discover.map.ui.adapter.BannerRvAdapter
+import com.mredrock.cyxbs.discover.map.ui.adapter.BannerViewAdapter
 import com.mredrock.cyxbs.discover.map.ui.adapter.DetailAttributeRvAdapter
 import com.mredrock.cyxbs.discover.map.ui.adapter.DetailTagRvAdapter
+import com.mredrock.cyxbs.discover.map.util.BannerPageTransformer
 import com.mredrock.cyxbs.discover.map.viewmodel.MapViewModel
-import com.to.aboomy.pager2banner.IndicatorView
-import com.to.aboomy.pager2banner.ScaleInTransformer
 import kotlinx.android.synthetic.main.map_fragment_place_detail_container.*
 
 
@@ -45,18 +42,27 @@ class PlaceDetailBottomSheetFragment : Fragment() {
         /**
          * 初始化adapter和layoutManager
          */
-        val indicator = IndicatorView(context)
-                .setIndicatorColor(Color.DKGRAY)
-                .setIndicatorSelectorColor(Color.WHITE)
-                .setIndicatorRatio(1f) //ratio，默认值是1 ，也就是说默认是圆点，根据这个值，值越大，拉伸越长，就成了矩形，小于1，就变扁了呗
-                .setIndicatorRadius(2f) // radius 点的大小
-                .setIndicatorSelectedRatio(3f)
-                .setIndicatorSelectedRadius(2f)
-                .setIndicatorStyle(IndicatorView.IndicatorStyle.INDICATOR_BIG_CIRCLE)
-        val bannerAdapter = context?.let { BannerRvAdapter(it, mutableListOf()) }
-        map_banner_detail_image.setIndicator(indicator).setPageMargin(context?.dp2px(20f)
-                ?: 0, context?.dp2px(10f)
-                ?: 0).addPageTransformer(ScaleInTransformer()).adapter = bannerAdapter
+//        val indicator = IndicatorView(context)
+//                .setIndicatorColor(Color.DKGRAY)
+//                .setIndicatorSelectorColor(Color.WHITE)
+//                .setIndicatorRatio(1f) //ratio，默认值是1 ，也就是说默认是圆点，根据这个值，值越大，拉伸越长，就成了矩形，小于1，就变扁了呗
+//                .setIndicatorRadius(2f) // radius 点的大小
+//                .setIndicatorSelectedRatio(3f)
+//                .setIndicatorSelectedRadius(2f)
+//                .setIndicatorStyle(IndicatorView.IndicatorStyle.INDICATOR_BIG_CIRCLE)
+//        val bannerAdapter = context?.let { BannerRvAdapter(it, mutableListOf()) }
+//        map_banner_detail_image.setIndicator(indicator).setPageMargin(context?.dp2px(20f)
+//                ?: 0, context?.dp2px(10f)
+//                ?: 0).addPageTransformer(ScaleInTransformer()).adapter = bannerAdapter
+
+
+        val bannerViewAdapter = context?.let { BannerViewAdapter(it, mutableListOf()) }
+        map_banner_detail_image.adapter = bannerViewAdapter
+        map_banner_detail_image.offscreenPageLimit = 3
+        map_banner_detail_image.pageMargin = 15
+        map_banner_detail_image.setPageTransformer(true, BannerPageTransformer())
+        map_banner_detail_image.start()
+
 
         val flexBoxManager = FlexboxLayoutManager(context)
         flexBoxManager.flexWrap = FlexWrap.WRAP
@@ -79,15 +85,17 @@ class PlaceDetailBottomSheetFragment : Fragment() {
                     //数据绑定
                     mBinding.placeDetails = t
                     //数据传给adapter
-                    if (bannerAdapter != null) {
+                    if (bannerViewAdapter != null) {
                         if (t.images != null) {
-                            bannerAdapter.setList(t.images!!)
-                            bannerAdapter.notifyDataSetChanged()
-                            map_banner_detail_image.adapter = bannerAdapter
+                            bannerViewAdapter.setList(t.images!!)
+                            bannerViewAdapter.notifyDataSetChanged()
+                            map_banner_detail_image.adapter = bannerViewAdapter
+                            map_indicator_detail.setupWithViewPager(map_banner_detail_image)
                         } else {
-                            bannerAdapter.setList(listOf())
-                            bannerAdapter.notifyDataSetChanged()
-                            map_banner_detail_image.adapter = bannerAdapter
+                            bannerViewAdapter.setList(listOf())
+                            bannerViewAdapter.notifyDataSetChanged()
+                            map_banner_detail_image.adapter = bannerViewAdapter
+                            map_indicator_detail.setupWithViewPager(map_banner_detail_image)
                         }
                     }
                     if (tagAdapter != null && t.tags != null) {
